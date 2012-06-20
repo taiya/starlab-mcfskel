14e8df5ae01c8e8befa747fb8bc4a68e360a7075
Andrea Tagliasacchi <andrea.tagliasacchi@gmail.com>
2012-06-19 20:14:18 -0700

    Tweaks to ibraheem's commit

--- solvertest/solvertest.cpp
+++ solvertest/solvertest.cpp
@@ -1,6 +1,9 @@
 #include"solvertest.h"
 
 void solvertest::applyFilter(Model* /*model*/,RichParameterSet* /*parameters*/, StarlabDrawArea* /*drawarea*/){
+        
+    
+
 }
 
 Q_EXPORT_PLUGIN(solvertest)
